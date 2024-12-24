0f9f1097184f11ef46928fd7346dd995ba8a4a8e
BEN-HUR PEREIRA BORGES DOS SANTOS <138973267+botlorien@users.noreply.github.com>
2024-12-23 21:39:05 -0400

    Add files via upload

--- vector_nd.py
+++ vector_nd.py
@@ -2,13 +2,40 @@ from array import array
 import reprlib
 import math
 import operator
+import functools
 
 
 class Vector:
     typecode = 'd'
+    __match_args__ = ('x', 'y', 'z', 't')
 
     def __init__(self, components):
         self._components = array(self.typecode, components)
+
+    def __getattr__(self, name):
+        cls = type(self)
+        try:
+            pos = cls.__match_args__.index(name)
+        except ValueError:
+            pos = -1
+        if 0 <= pos < len(self._components):
+            return self._components[pos]
+        msg = f'{cls.__name__!r} object has no attribute {name!r}'
+        raise AttributeError(msg)
+
+    def __setattr__(self, name, value):
+        cls = type(self)
+        if len(name) == 1:
+            if name in cls.__match_args__:
+                error = 'readonly attribute {attr_name!r}'
+            elif name.islower():
+                error = "can't set attributes 'a' to 'z' in {cls_name!r}"
+            else:
+                error = ''
+            if error:
+                msg = error.format(cls_name=cls.__name__, attr_name=name)
+                raise AttributeError(msg)
+        super().__setattr__(name, value)
 
     def __iter__(self):
         return iter(self._components)
@@ -26,24 +53,28 @@ class Vector:
                 bytes(self._components)
                 )
 
-    def __eq__(self, other):
-        return tuple(self) == tuple(other)
-
     def __abs__(self):
         return math.hypot(*self)
 
     def __bool__(self):
         return bool(abs(self))
-    
+
     def __len__(self):
         return len(self._components)
-    
+
     def __getitem__(self, key):
         if isinstance(key, slice):
             cls = type(self)
             return cls(self._components[key])
         index = operator.index(key)
         return self._components[index]
+
+    def __eq__(self, other):
+        return len(self) == len(other) and all(a == b for a, b in zip(self, other))
+
+    def __hash__(self):
+        hashes = (hash(x) for x in self._components)
+        return functools.reduce(operator.xor, hashes, 0)
 
     @classmethod
     def frombytes(cls, octets):
@@ -71,5 +102,13 @@ if __name__ == '__main__':
     print(repr(v3[-1]))
     print(repr(v3[-1:]))
     print(repr(v3[1:4]))
-    print(repr(v3[6.0]))
-    print(repr(v3[1,2]))
+    print(v3.x, v3.y, v3.z, v3.t)
+    try:
+        v3.x = 10
+    except AttributeError as e:
+        print(e)
+
+    try:
+        v3.a = 10
+    except AttributeError as e:
+        print(e)
