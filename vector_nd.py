6273226a141de26c85e665d791aafd66ef6c7a98
BEN-HUR PEREIRA BORGES DOS SANTOS <138973267+botlorien@users.noreply.github.com>
2025-01-02 17:31:57 -0400

    Update vector_nd.py

--- vector_nd.py
+++ vector_nd.py
@@ -3,6 +3,7 @@ import reprlib
 import math
 import operator
 import functools
+import itertools
 
 
 class Vector:
@@ -11,6 +12,46 @@ class Vector:
 
     def __init__(self, components):
         self._components = array(self.typecode, components)
+
+    def __iter__(self):
+        return iter(self._components)
+
+    def __repr__(self):
+        components = reprlib.repr(self._components)
+        components = components[components.find('['):-1]
+        return f'Vector({components})'
+
+    def __str__(self):
+        return str(tuple(self))
+
+    def __bytes__(self):
+        return (bytes([ord(self.typecode)]) +
+                bytes(self._components)
+                )
+    
+    def __eq__(self, other):
+        return (len(self) == len(other) and
+                all(a == b for a, b in zip(self, other)))
+
+    def __hash__(self):
+        hashes = (hash(x) for x in self._components)
+        return functools.reduce(operator.xor, hashes, 0)
+    
+    def __abs__(self):
+        return math.hypot(*self)
+
+    def __bool__(self):
+        return bool(abs(self))
+
+    def __len__(self):
+        return len(self._components)
+
+    def __getitem__(self, key):
+        if isinstance(key, slice):
+            cls = type(self)
+            return cls(self._components[key])
+        index = operator.index(key)
+        return self._components[index]
 
     def __getattr__(self, name):
         cls = type(self)
@@ -37,44 +78,27 @@ class Vector:
                 raise AttributeError(msg)
         super().__setattr__(name, value)
 
-    def __iter__(self):
-        return iter(self._components)
+    def angle(self, n):
+        r = math.hypot(*self[n:])
+        a = math.atan2(r, self[n-1])
+        if (n == len(self) - 1) and (self[-1] < 0):
+            return math.pi * 2 - a
+        else:
+            return a
 
-    def __repr__(self):
-        components = reprlib.repr(self._components)
-        components = components[components.find('['):-1]
-        return f'Vector({components})'
+    def angles(self):
+        return (self.angle(n) for n in range(1, len(self)))
 
-    def __str__(self):
-        return str(tuple(self))
-
-    def __bytes__(self):
-        return (bytes([ord(self.typecode)]) +
-                bytes(self._components)
-                )
-
-    def __abs__(self):
-        return math.hypot(*self)
-
-    def __bool__(self):
-        return bool(abs(self))
-
-    def __len__(self):
-        return len(self._components)
-
-    def __getitem__(self, key):
-        if isinstance(key, slice):
-            cls = type(self)
-            return cls(self._components[key])
-        index = operator.index(key)
-        return self._components[index]
-
-    def __eq__(self, other):
-        return len(self) == len(other) and all(a == b for a, b in zip(self, other))
-
-    def __hash__(self):
-        hashes = (hash(x) for x in self._components)
-        return functools.reduce(operator.xor, hashes, 0)
+    def __format__(self, fmt_spec=''):
+        if fmt_spec.endswith('h'):
+            fmt_spec = fmt_spec[:-1]
+            coords = itertools.chain([abs(self)], self.angles())
+            outer_fmt = '<{}>'
+        else:
+            coords = self
+            outer_fmt = '({})'
+        components = (format(c, fmt_spec) for c in coords)
+        return outer_fmt.format(', '.join(components))
 
     @classmethod
     def frombytes(cls, octets):
